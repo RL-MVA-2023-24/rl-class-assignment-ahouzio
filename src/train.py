bee9e2559aa63bdd787f8cf3ecde004f6c2c6642
Oussama Ahouzi <70753395+Ousama-gif@users.noreply.github.com>
2024-03-02 20:14:24 +0100

    debug

--- src/train.py
+++ src/train.py
@@ -59,7 +59,7 @@ class ProjectAgent:
         
     def load(self):
         self.agent = dqn_agent(config, DQN)
-        path = os.getcwd() + "/src/models/model_ep_1000.pt"
+        path = os.getcwd() + "/src/model_ep_1000.pt"
         self.agent.model.load_state_dict(torch.load(path, 
                                                     map_location= torch.device('cpu')))
         
